86f3032dbe3d16f7af866506d892e65fa7eb8866
Alexandre Duteau <alex@midaslabs.co>
2024-08-31 17:21:00 -0600

    Changed font, added landing page,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { EB_Garamond } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = EB_Garamond({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: " Alexandre Duteau",
-  description: "Alexandre Duteau's personal website",
+  description: "Alexandre Duteau's Portfolio",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "./globals.css";
 import {NavBar} from "@/components/navbar";
 import { AboutTitle } from "@/components/aboutTitle";
 import { AboutSection } from "@/components/aboutSection";
@@ -7,6 +8,7 @@ import { SkillSection } from "@/components/skillsSection";
 import { ProjectTitle } from "@/components/projectTitle";
 import { ProjectSection } from "@/components/projectSection";
 import { Footer } from "@/components/footer";
+import { EB_Garamond } from "next/font/google";
 
 export default function Home() {
   return (
@@ -14,6 +16,16 @@ export default function Home() {
 
     <div className="bg-[#131313]">
       <NavBar />
+      <div className="flex flex-col justify-center items-center text-center w-screen md:w-full h-screen landing-page-bg bg-cover bg-no-repeat mb-20">
+        <div className="flex flex-col items-center justify-center text-center text-white h-screen px-4">
+          <h1 className="text-3xl md:text-4xl font-bold mb-6">Alexandre Duteau</h1>
+          <h1 className="text-xl md:text-2xl font-bold mb-3">Student </h1>
+          <h1 className="text-xl md:text-2xl font-bold mb-3">Developer</h1>
+          <h1 className="text-xl md:text-2xl font-bold mb-3">Innovator</h1>
+        </div>
+      </div>
+      
+      {/* <LandingPage /> */}
       <AboutTitle />
       <AboutSection />
       <SkillsTitle />
