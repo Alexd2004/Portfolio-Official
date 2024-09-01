8c7043a3f29b87faace588f704f0aff12a50da82
Alexandre Duteau <alex@midaslabs.co>
2024-08-31 18:41:27 -0600

    updated margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
   return (
 
 
-    <div className="bg-[#131313]">
+    <div className="bg-[#141414]">
       <NavBar />
-      <div className="flex flex-col justify-center items-center text-center w-screen md:w-full h-screen landing-page-bg bg-cover bg-no-repeat mb-20">
+      <div className="flex flex-col justify-center items-center text-center w-screen md:w-full h-screen landing-page-bg bg-cover bg-no-repeat mb-10">
         <div className="flex flex-col items-center justify-center text-center text-white h-screen px-4">
           <h1 className="text-3xl md:text-4xl font-bold mb-6">Alexandre Duteau</h1>
           <h1 className="text-xl md:text-2xl font-bold mb-3">Student </h1>
